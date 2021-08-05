3ec17389df3885dac86fa482fb04ee38866ad9ed
Matt <home1@apples-mbp.home>
2021-08-05 11:38:55 +0100

    form.js amend

--- components/Form.js
+++ components/Form.js
@@ -145,7 +145,6 @@ export default function Form(props) {
                             variant="contained"
                             color="primary"
                             type='submit'
-                            className={classes.button}
                             endIcon={<SendIcon/>}
                         >
                             Send
