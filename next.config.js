8c3ae5d569a4473ba0076e1bc0a9eece33e2592e
Matt <home1@apples-mbp.home>
2021-08-05 11:21:01 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+}
+
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(png|svg|jpg|gif|pdf)$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: 'file-loader',
+          options: {
+          name: '[name].[ext]'
+          }
+        },
+      ],
+    })
+
+    return config
+  },
 }
