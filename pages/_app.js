c4530053a2c00ee286c1fa8d875ae4c64f47c0a0
Matt <home1@Apples-MacBook-Pro.local>
2021-11-17 08:49:26 +0000

    new home page 2

--- pages/_app.js
+++ pages/_app.js
@@ -27,7 +27,7 @@ class MyApp extends App {
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
           <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"></link>
         </Head>
         <ThemeProvider theme={theme}>
